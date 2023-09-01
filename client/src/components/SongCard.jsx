a323a117e7dab4b7ac7a2b2a9c51912c8a8ecbda
bhavya.vats05@gmail.com <108180977+bhavya-vats@users.noreply.github.com>
2023-09-01 15:00:57 +0530

    new

--- client/src/components/SongCard.jsx
+++ client/src/components/SongCard.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
 const Songcard = ({data, index}) => {
   return (
-    <motion.div>Songcard</motion.div>
+    <motion.div className="relative w-40 min-w-210 px-2 cursor-pointer hover:bg-card bg-gray-100 shadow-md rounded-lg flex flex-col items-center">
+      <div>
+        <motion.img src={data.imageURL}/>
+      </div>
+    </motion.div>
   )
 }
 
