a6102c0ad2010361f212d945300e609c3803d324
bhavya.vats05@gmail.com <108180977+bhavya-vats@users.noreply.github.com>
2023-09-05 22:12:22 +0530

    new

--- client/src/components/FilterButtons.jsx
+++ client/src/components/FilterButtons.jsx
@@ -1,8 +1,35 @@
 import React, { useEffect, useState } from "react";
+import { IoChevronDown } from "react-icons/io5";
+import { motion } from "framer-motion";
 
-const FilterButtons = () => {
+const FilterButtons = ({ filterData, flag }) => {
+    const [filterName, setFilterName] = useState(null);
+    const [filterMenu, setFilterMenu] = useState(false);
+
   return (
     <div className="border border-gray-300 rounded-md px-4 py-1 relative cursor-pointer hover:border-gray-400">
+         <p className="text-base tracking-wide text-textColor flex items-center gap-2" 
+         onClick={() => setFilterMenu(!filterMenu)}> {!filterName && flag}
+            <IoChevronDown className={`text-base text-textColor duration-150 transition-all ease-in-out ${
+            filterMenu ? "rotate-180" : "rotate-0"
+          }` } />
+         </p>
+         {filterData && filterMenu && (
+         <motion.div
+//           initial={{ opacity: 0, y: 50 }}
+//           animate={{ opacity: 1, y: 0 }}
+//           exit={{ opacity: 0, y: 50 }}
+          className="w-48 z-50 backdrop-blur-sm max-h-44 overflow-y-scroll scrollbar-thin scrollbar-track-gray-200 scrollbar-thumb-gray-400 py-2 flex flex-col rounded-md shadow-md absolute top-8 left-0">
+            {filterData?.map((data) => (
+            <div
+              key={data.name}
+              className="flex items-center gap-2 px-4 py-1 hover:bg-gray-200"
+            //   onClick={() => updateFilterButton(data.name)}
+    >
+                </div>
+            ))}
+        </motion.div>
+         )}
         </div>
   )
 }
@@ -10,15 +37,15 @@ const FilterButtons = () => {
 export default FilterButtons
 
 // 
-// import { IoChevronDown } from "react-icons/io5";
+// 
 
-// import { motion } from "framer-motion";
+// 
 // import { useStateValue } from "../Context/StateProvider";
 // import { actionType } from "../Context/reducer";
 
-// const FilterButtons = ({ filterData, flag }) => {
-//   const [filterName, setFilterName] = useState(null);
-//   const [filterMenu, setFilterMenu] = useState(false);
+// const FilterButtons = () => {
+//   
+//   
 
 //   const [{ artistFilter, albumFilter, filterTerm }, dispatch] = useStateValue();
 
@@ -44,11 +71,8 @@ export default FilterButtons
 
 //   return (
 //     
-//       <p
-//         className="text-base tracking-wide text-textColor flex items-center gap-2 "
-//         onClick={() => setFilterMenu(!filterMenu)}
-//       >
-//         {!filterName && flag}
+//      
+//         
 //         {filterName && (
 //           <>
 //             {filterName.length > 15
@@ -56,25 +80,11 @@ export default FilterButtons
 //               : filterName}
 //           </>
 //         )}
-//         <IoChevronDown
-//           className={`text-base text-textColor duration-150 transition-all ease-in-out ${
-//             filterMenu ? "rotate-180" : "rotate-0"
-//           }`}
+//          `}
 //         />
 //       </p>
-//       {filterData && filterMenu && (
-//         <motion.div
-//           initial={{ opacity: 0, y: 50 }}
-//           animate={{ opacity: 1, y: 0 }}
-//           exit={{ opacity: 0, y: 50 }}
-//           className="w-48 z-50 backdrop-blur-sm max-h-44 overflow-y-scroll scrollbar-thin scrollbar-track-gray-200 scrollbar-thumb-gray-400 py-2 flex flex-col rounded-md shadow-md absolute top-8 left-0"
-//         >
-//           {filterData?.map((data) => (
-//             <div
-//               key={data.name}
-//               className="flex items-center gap-2 px-4 py-1 hover:bg-gray-200"
-//               onClick={() => updateFilterButton(data.name)}
-//             >
+//       
+//           
 //               {(flag === "Artist" || flag === "Albums") && (
 //                 <img
 //                   src={data.imageURL}
